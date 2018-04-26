b11253df837052cd5039be009113b4554960df5a
aback1227 <aback1227@gmail.com>
2018-04-26 13:18:06 -0400

    create listings table and listing model file

---
+++ db/migrate/20180426171148_create_listings.rb
@@ -0,0 +1,11 @@
+class CreateListings < ActiveRecord::Migration[5.0]
+  def change
+    create_table :listings do |t|
+      t.string :unit_number
+      t.string :unit_type
+      t.string :sq_feet
+      t.string :rent_cost
+      t.string :availability
+    end
+  end
+end
