528f0f2658f9c80f03283fd7e2cbc2643a91945e
aback1227 <aback1227@gmail.com>
2018-04-26 16:14:05 -0400

    add listing data into location.listings array

--- app/controllers/scraper_controller.rb
+++ app/controllers/scraper_controller.rb
@@ -8,8 +8,9 @@ class ScraperController < ApplicationController
     b.goto "https://www.livearia.com/availability"
     doc ||= Nokogiri::HTML(b.html)
     available_units = doc.css('.available-unit a')
+    location = Location.create(city_state: "Cerritos, CA")
     available_units.each do |unit|
-      Listing.create(
+      listing = Listing.create(
         unit_number: unit.css('.listing-unit-num').text, 
         unit_type: unit.css('.listing-unit-info').children[0].text, 
         sq_feet: unit.css('.listing-unit-info').children[4].text.split(" ")[0], 
@@ -17,6 +18,7 @@ class ScraperController < ApplicationController
         availability: unit.css('.listing-unit-date').text,
         source: "Aria"
       )
+      location.listings.push(listing)
     end
   end
 end
