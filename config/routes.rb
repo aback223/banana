7b9ff5b0983700bbd01c958f0310a228d7a0eabf
aback1227 <aback1227@gmail.com>
2018-04-26 16:56:25 -0400

    add an index route for location

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'listing#index'
+  resources :location, only: :index
+
   get '/scraper', to: 'scraper#scrape', as: 'scrape'
 end
