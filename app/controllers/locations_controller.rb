2e31bd73a68053c699ffed3f51cc155f6a5db105
aback1227 <aback1227@gmail.com>
2018-04-27 11:14:30 -0400

    remove Day.uniqe_dates from controller and location index

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -2,7 +2,6 @@ class LocationsController < ApplicationController
   def index
     @locations = Location.all
     @listings = Listing.sort_avail_then_floorplan
-    @days = Day.unique_dates
     respond_to do |format|
       format.html {render :index}
       format.json {render json: @locations}
