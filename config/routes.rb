6ce7b9bdd79104cd85ed7198a93ee29a442e7fd7
aback1227 <aback1227@gmail.com>
2018-04-26 22:59:48 -0400

    add route for days index

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'listings#index'
   resources :locations, only: :index
+  resources :days, only: :index
 
   get '/scraper', to: 'scraper#scrape'
   get '/scrape_data', to: 'scraper#scrape_data'
