8ff41c63af00239cf68e6304c63ed7adb3666d1a
aback1227 <aback1227@gmail.com>
2018-04-26 22:59:23 -0400

    class method for finding unique dates and render to location index

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,6 +1,7 @@
 class LocationsController < ApplicationController
   def index
     @locations = Location.all
+    @days = Day.unique_dates
     respond_to do |format|
       format.html {render :index}
       format.json {render json: @locations, include: '**'}
--- app/models/day.rb
+++ app/models/day.rb
@@ -1,3 +1,7 @@
 class Day < ActiveRecord::Base
   belongs_to :listing
+
+  def self.unique_dates
+    Day.select(:date).uniq
+  end
 end
