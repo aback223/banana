a0cd420470df84df5c11b1c0661a0fd0a477c694
aback1227 <aback1227@gmail.com>
2018-04-27 01:06:53 -0400

    @locations instance variable is sorted by avail then floorplan

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,6 +1,7 @@
 class LocationsController < ApplicationController
   def index
     @locations = Location.all
+    @listings = Listing.sort_avail_then_floorplan
     @days = Day.unique_dates
     respond_to do |format|
       format.html {render :index}
