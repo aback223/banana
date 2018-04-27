ae9694b3ad8507e671402d67044118dd5f2f2218
aback1227 <aback1227@gmail.com>
2018-04-27 11:44:09 -0400

    Location instance method for retrieving unique dates

--- app/models/Location.rb
+++ app/models/Location.rb
@@ -1,4 +1,9 @@
 class Location < ActiveRecord::Base
   has_many :listings
   has_many :days, through: :listings
+
+  def get_unique_dates
+    location = Location.find_by(id: self.id)
+    location.days.select(:date).distinct
+  end
 end
