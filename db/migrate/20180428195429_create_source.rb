5b870bc16ff91869998ec9fa70f261ef41221977
aback1227 <aback1227@gmail.com>
2018-04-28 15:56:03 -0400

    create a source table and model

---
+++ db/migrate/20180428195429_create_source.rb
@@ -0,0 +1,7 @@
+class CreateSource < ActiveRecord::Migration[5.0]
+  def change
+    create_table :sources do |t|
+      t.string :name
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180428040420) do
+ActiveRecord::Schema.define(version: 20180428195429) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,10 @@ ActiveRecord::Schema.define(version: 20180428040420) do
 
   create_table "locations", force: :cascade do |t|
     t.string "city_state"
+  end
+
+  create_table "sources", force: :cascade do |t|
+    t.string "name"
   end
 
 end
