cb3b4004c02c26a8ae79580aec2e595d5e8f29dd
aback1227 <aback1227@gmail.com>
2018-04-28 15:48:21 -0400

    start working on the to_csv method

---
+++ app/models/csv_export.rb
@@ -0,0 +1,18 @@
+class CsvExport < ActiveRecord::Base
+
+  def self.to_csv(options: {})
+    CSV.generate(options) do |csv|
+      csv.add_row header_names          #header
+      all.each do |foo|                 #row value iteration
+        values = foo.attributes.values
+        csv.add_row values 
+      end
+    end
+  end
+
+  def self.header_names
+    header = ["Property", "Unit Type", "Floor Plan", "Unit #", "SF", "Status"]
+    date_headers = Day.get_unique_dates #descending order
+    header + date_headers
+  end
+end
