897f186240395f5c6ccdc6bf921b946e2f356c7a
aback1227 <aback1227@gmail.com>
2018-04-27 22:57:02 -0400

    calc_avg class method in Day

--- app/models/day.rb
+++ app/models/day.rb
@@ -1,3 +1,43 @@
 class Day < ActiveRecord::Base
   belongs_to :listing
+
+  def self.floorplan_hash
+    hash = {
+      "S1": [], 
+      "S2": [], 
+      "A1": [], 
+      "A2": [], 
+      "A3": [], 
+      "A4": [], 
+      "B1": [], 
+      "B2": [], 
+      "B3": [], 
+      "B4": [], 
+      "S2L": [], 
+      "A1L": [], 
+      "A2L": [], 
+      "A3L": [], 
+      "B2L": [], 
+      "B3L": [], 
+      "B4L": []
+    }
+    hash.stringify_keys!
+  end
+
+  def self.calcAvg
+    hash = Day.floorplan_hash
+    hash.each do |key, value|
+      Day.all.each do |day|
+        if key == day.listing.floorplan && day.date == Date.today.strftime("%m/%d/%Y")
+          hash[key] << day.rent.tr('$', '').to_i
+        end
+      end
+    end
+    hash.each do |key, value|
+      if !value.empty?
+        hash[key] = (value.inject(:+)/value.size).round
+      end
+    end
+    hash
+  end
 end
