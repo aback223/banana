3f8d18c93368663218c3833fc1593e506f8015f4
aback1227 <aback1227@gmail.com>
2018-04-28 00:23:52 -0400

    add a calc and save totals to DailyData table

--- app/models/daily_data.rb
+++ app/models/daily_data.rb
@@ -1,2 +1,9 @@
 class DailyData < ActiveRecord::Base
+  def self.calcAndSave
+    if DailyData.where(date: Date.today.strftime("%m/%d/%Y")).empty?
+      occupancy = Listing.calc_ttlVacantPct
+      leased = Listing.ttlExpPct
+      new = DailyData.create(date: Date.today.strftime("%m/%d/%Y"), occupancy: occupancy, leased: leased)
+    end
+  end
 end
