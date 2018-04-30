8742c46f410dee10a81f42840807815ecbec8202
aback1227 <aback1227@gmail.com>
2018-04-29 20:36:04 -0400

    enable render of json for listings index view

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,5 +1,9 @@
 class ListingsController < ApplicationController
   def index
     @listings = Listing.all
+    respond_to do |format|
+      format.html {render :index}
+      format.json {render json: @listings}
+    end
   end
 end
