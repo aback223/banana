ea99b93c6e90985aa090bba76415791148a0be6a
aback1227 <aback1227@gmail.com>
2018-05-02 19:10:17 -0400

    create a csv export controller

--- app/controllers/csv_export_controller.rb
+++ app/controllers/csv_export_controller.rb
@@ -1,6 +1,6 @@
 class CsvExportController < ApplicationController
   def export
-    @data 
+    @data = Listing.all
     respond_to do |format|
       format.html { redirect_to root_url}
       format.csv {send_data @data.to_csv}
