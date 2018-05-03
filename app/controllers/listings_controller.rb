198a44ae793935f4d00052c6cd0f044677469bf7
aback1227 <aback1227@gmail.com>
2018-05-02 21:53:07 -0400

    @listings = Listing.all

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,6 +1,6 @@
 class ListingsController < ApplicationController
   def index
-    @listings = Listing.sort_avail_then_floorplan
+    @listings = Listing.all
     respond_to do |format|
       format.html {render :index}
       format.json {render json: @listings}
